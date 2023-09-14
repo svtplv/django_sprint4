a98f8f8270ca064681617e0bc28bab13c299cec0
Святослав Поляков <viewtifulmadguy@gmail.com>
2023-09-14 15:58:04 +0300

    Change indent

--- blogicum/blog/utils.py
+++ blogicum/blog/utils.py
@@ -6,11 +6,10 @@ from blog.models import Post
 
 def get_query_posts(published=True):
     queryset = Post.objects.prefetch_related(
-            'location',
-            'author',
-            'category').annotate(
-                comment_count=Count('comments')
-                ).order_by('-pub_date')
+        'location',
+        'author',
+        'category').annotate(
+            comment_count=Count('comments')).order_by('-pub_date')
     if published:
         return queryset.filter(
             pub_date__lte=timezone.now(),
